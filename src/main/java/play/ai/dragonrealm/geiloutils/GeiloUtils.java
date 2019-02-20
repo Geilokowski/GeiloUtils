03cd29971887328797397d25bdbe2b6378525916
dmf444 <becat444@gmail.com>
2019-02-19 21:50:33 -0500

    Fixes Building with mcmod.info and Forge registered version

--- src/main/java/play/ai/dragonrealm/geiloutils/GeiloUtils.java
+++ src/main/java/play/ai/dragonrealm/geiloutils/GeiloUtils.java
@@ -35,7 +35,7 @@ public class GeiloUtils
 {
 	  public static final String MODID = "geiloutils";
 	  public static final String NAME = "GeiloUtils";
-	  public static final String VERSION = "1.3.3";
+	  public static final String VERSION = "@VERSION@";
 	  private static Logger logger;
 	  
 	  @EventHandler
